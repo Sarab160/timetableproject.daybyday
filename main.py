9b97930e77e12ba80b8b97b4420ce2e58b922dac
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2025-07-12 06:42:12 -0700

    day 06

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from datetime import datetime,timedelta
 import json
 import customtkinter as ctk
 from tkinter import filedialog
-
+from tkinter import messagebox
 from fpdf import FPDF
+
 with open("teachers.json") as t:
     teacher=json.load(t)
 
@@ -224,14 +225,14 @@ def export_pdf(timetable, selectedClasses, break_period):
     )
 
     if not file_path:
-        output.configure(text="❌ PDF export cancelled.")
+        messagebox.showerror("Error","❌ PDF export cancelled.")
         return
 
     try:
         user_start_time = start_time_input.get() or "08:00"
         user_duration = int(duration_input.get() or "45")
     except:
-        output.configure(text="❌ Invalid start time or duration.")
+        messagebox.showerror("Error","Invalid start time or duration")
         return
 
     pdf = TimetablePDF('L', 'mm', 'A4')
@@ -290,7 +291,7 @@ def export_pdf(timetable, selectedClasses, break_period):
 
     try:
         pdf.output(file_path)
-        output.configure(text=f"✅ PDF saved to: {file_path}")
+        messagebox.showinfo(text=f"✅ PDF saved to: {file_path}")
     except Exception as e:
         output.configure(text=f"❌ Failed to save PDF: {str(e)}")
 
@@ -309,41 +310,230 @@ app=ctk.CTk()
 app.geometry("1000x750")
 app.title("TimelyAI")
 
-ctk.CTkLabel(app, text="Enter Classes (comma separated):").pack(pady=5)
-class_input = ctk.CTkEntry(app, placeholder_text="A,B,C", width=400)
+home_frame=ctk.CTkFrame(app)
+setup_frame=ctk.CTkFrame(app)
+timetable_fame=ctk.CTkFrame(app)
+info_frame=ctk.CTkFrame(app)
+teacher_frame=ctk.CTkFrame(app)
+class_frame=ctk.CTkFrame(app)
+subjects_frame=ctk.CTkFrame(app)
+room_frame=ctk.CTkFrame(app)
+frames=[setup_frame,timetable_fame,home_frame,info_frame,teacher_frame,room_frame,class_frame,subjects_frame]
+
+def showframe(frame):
+    for f in frames:
+        f.place_forget()
+    frame.place(relwidth=1, relheight=1)
+    
+spacer = ctk.CTkFrame(home_frame, height=100, fg_color="transparent")
+h_button_config = {
+    "font": ctk.CTkFont(size=20, weight="bold"),
+    "corner_radius": 12,
+    "width": 250,
+    "height": 60,
+    "fg_color": "#007BFF",        
+    "hover_color": "#0056d2",     
+    "border_color": "#0056d2",    
+ 
+}
+button_config = {
+    "font": ctk.CTkFont(size=14, weight="bold"),
+    "corner_radius": 10,
+    "width": 200,
+    "height": 45,
+    "fg_color": "#007BFF",        
+    "hover_color": "#0056d2",     
+    "border_color": "#0056d2", 
+}
+
+ctk.CTkLabel(setup_frame,text="Setup TimeTable",font=("Arial",20,"bold")).pack(pady=10)
+ctk.CTkLabel(home_frame, text="⏰ TimelyAI 🤖", font=ctk.CTkFont(size=30, weight="bold"), text_color="white").pack(pady=10)
+
+
+ctk.CTkLabel(home_frame,text="Home Page",font=ctk.CTkFont(size=15, weight="bold"), text_color="white").pack(pady=0,padx=10)
+
+spacer.pack()
+spacer.pack()
+spacer.pack()
+spacer.pack()
+button_container = ctk.CTkFrame(home_frame, fg_color="transparent")
+button_container.pack(pady=(40,10))
+setup_button = ctk.CTkButton(button_container, text="📅 Setup Timetable", command=lambda: showframe(setup_frame),**h_button_config)
+setup_button.pack(pady=10)
+
+ctk.CTkLabel(setup_frame, text="Enter Classes (comma separated):").pack(pady=5)
+class_input = ctk.CTkEntry(setup_frame, placeholder_text="A,B,C", width=400)
 class_input.pack(pady=5)
 
-ctk.CTkLabel(app, text="Start Time (HH:MM):").pack(pady=5)
-start_time_input = ctk.CTkEntry(app, placeholder_text="08:00", width=200)
+ctk.CTkLabel(setup_frame, text="Start Time (HH:MM):").pack(pady=5)
+start_time_input = ctk.CTkEntry(setup_frame, placeholder_text="08:00", width=200)
 start_time_input.pack(pady=5)
 
-ctk.CTkLabel(app, text="Lecture Duration (minutes):").pack(pady=5)
-duration_input = ctk.CTkEntry(app, placeholder_text="45", width=200)
+ctk.CTkLabel(setup_frame, text="Lecture Duration (minutes):").pack(pady=5)
+duration_input = ctk.CTkEntry(setup_frame, placeholder_text="45", width=200)
 duration_input.pack(pady=5)
-ctk.CTkLabel(app, text="Break Period Number (e.g., 3):").pack(pady=5)
-break_period_input = ctk.CTkEntry(app, placeholder_text="3", width=200)
+
+ctk.CTkLabel(setup_frame, text="Break Period Number (e.g., 3):").pack(pady=5)
+break_period_input = ctk.CTkEntry(setup_frame, placeholder_text="3", width=200)
 break_period_input.pack(pady=5)
-load_subject_btn = ctk.CTkButton(app, text="Load Subjects", command=lambda: subject_entites(class_input.get().split(",")))
+
+load_subject_btn = ctk.CTkButton(setup_frame, text="📚 Load Subjects", command=lambda: subject_entites(class_input.get().split(",")),**button_config)
 load_subject_btn.pack(pady=10)
 
-subject_frame = ctk.CTkScrollableFrame(app, height=200, width=950)
+subject_frame = ctk.CTkScrollableFrame(setup_frame, height=200, width=950)
 subject_frame.pack(pady=10)
 
-generate_btn = ctk.CTkButton(app, text="Generate Timetable", command=generate_timable)
+ctk.CTkButton(setup_frame, text="⬅️ Back to Home", command=lambda: showframe(home_frame),**button_config).pack(pady=10)
+
+generatetime_btn = ctk.CTkButton(button_container, text="⚙️ Generate Timetable", command=lambda:showframe(timetable_fame),**h_button_config)
+generatetime_btn.pack(pady=10)
+
+ctk.CTkLabel(timetable_fame,text="⚙️ Generate Timetable and Export as Pdf",font=("Aial",20,"bold")).pack(pady=10)
+
+generate_btn = ctk.CTkButton(timetable_fame, text="⚙️ Generate Timetable", command=generate_timable,**button_config)
 generate_btn.pack(pady=10)
+
 export_pdf_btn = ctk.CTkButton(
-    app,
-    text="Export as PDF",
-    command=lambda: export_pdf(timetable, selected_classes,break_period)
+    timetable_fame,
+    text="📄 Export as PDF",
+    command=lambda: export_pdf(timetable, selected_classes,break_period),**button_config
 )
+
 export_pdf_btn.pack(pady=10)
-reset_display_btn = ctk.CTkButton(app, text="Clear Timetable Display", command=clear_timetable_display)
+
+reset_display_btn = ctk.CTkButton(timetable_fame, text="❌ Clear Timetable Display", command=clear_timetable_display,**button_config)
 reset_display_btn.pack(pady=10)
-output = ctk.CTkLabel(app, text="")
+
+output = ctk.CTkLabel(timetable_fame, text="")
 output.pack()
 
-output_textbox = ctk.CTkTextbox(app, width=950, height=300, font=("Courier", 12))
+output_textbox = ctk.CTkTextbox(timetable_fame, width=950, height=400, font=("Courier", 12))
 output_textbox.pack(pady=10)
 output_textbox.configure(state="disabled")
+ctk.CTkButton(timetable_fame, text="⬅️ Back to Home", command=lambda: showframe(home_frame),**button_config).pack(pady=10)
 
+
+
+
+
+# details of data
+
+def show_teachers_data():
+    # Clear previous content in findroute_frame
+    for widget in teacher_frame.winfo_children():
+        widget.destroy()
+
+    ctk.CTkLabel(teacher_frame, text="All Teachers Info", font=("Arial", 16, "bold")).pack(pady=10)
+
+    scroll_frame = ctk.CTkScrollableFrame(teacher_frame, width=760, height=460)
+    scroll_frame.pack(padx=10, pady=5, fill="both", expand=True)
+
+    for tid, info in teacher.items():
+        frame = ctk.CTkFrame(scroll_frame)
+        frame.pack(pady=5, padx=10, fill="x")
+
+        name = info.get("name", "N/A")
+        subjects_str = ", ".join(info.get("subjects", []))
+        unavailable = ", ".join(info.get("preferences", {}).get("unavailable_days", []))
+
+        ctk.CTkLabel(frame, text=f"ID: {tid} | Name: {name}", font=("Arial", 12, "bold")).pack(anchor="w", padx=10)
+        ctk.CTkLabel(frame, text=f"Subjects: {subjects_str}").pack(anchor="w", padx=20)
+        ctk.CTkLabel(frame, text=f"Unavailable Days: {unavailable if unavailable else 'None'}").pack(anchor="w", padx=20)
+
+    ctk.CTkButton(teacher_frame, text="⬅️ Back to Details", command=lambda: showframe(info_frame),**button_config).pack(pady=10)
+
+def show_class_data():
+    # Clear previous content in findroute_frame
+    for widget in class_frame.winfo_children():
+        widget.destroy()
+
+    ctk.CTkLabel(class_frame, text="All Teachers Info", font=("Arial", 16, "bold")).pack(pady=10)
+
+    scroll_frame = ctk.CTkScrollableFrame(class_frame, width=760, height=460)
+    scroll_frame.pack(padx=10, pady=5, fill="both", expand=True)
+
+    for tid, info in classes.items():
+        frame = ctk.CTkFrame(scroll_frame)
+        frame.pack(pady=5, padx=10, fill="x")
+
+        name = info.get("name", "N/A")
+        subjects_str = ", ".join(info.get("subjects", []))
+        
+
+        ctk.CTkLabel(frame, text=f"=> {tid} | Class Name: {name}", font=("Arial", 12, "bold")).pack(anchor="w", padx=10)
+        ctk.CTkLabel(frame, text=f"Subjects: {subjects_str}").pack(anchor="w", padx=20)
+        
+
+    ctk.CTkButton(class_frame, text="⬅️ Back to Details", command=lambda: showframe(info_frame),**button_config).pack(pady=10)
+
+def show_subject_data():
+    # Clear previous content in subject_frame
+    for widget in subjects_frame.winfo_children():
+        widget.destroy()
+
+    # 🔘 Heading
+    ctk.CTkLabel(subjects_frame, text="📘 All Subject Information", font=("Arial", 18, "bold")).pack(pady=10)
+
+    # 🔃 Scrollable section
+    scroll_frame = ctk.CTkScrollableFrame(subjects_frame, width=760, height=460)
+    scroll_frame.pack(padx=10, pady=5, fill="both", expand=True)
+
+    for sub_id, info in subject.items():
+        frame = ctk.CTkFrame(scroll_frame)
+        frame.pack(pady=5, padx=10, fill="x")
+
+        # Extract type
+        sub_type = info.get("type", "N/A")
+
+        # Duration
+        dur_raw = info.get("duration", "N/A")
+        duration = ", ".join(map(str, dur_raw)) if isinstance(dur_raw, (list, tuple)) else str(dur_raw)
+
+        # Weekly lectures
+        lect_raw = info.get("weekly_lectures", "N/A")
+        lectures = ", ".join(map(str, lect_raw)) if isinstance(lect_raw, (list, tuple)) else str(lect_raw)
+
+        
+        ctk.CTkLabel(frame, text=f"=> {sub_id}  |  Type: {sub_type}", font=("Arial", 13, "bold")).pack(anchor="w", padx=10)
+        ctk.CTkLabel(frame, text=f" Lecture Duration: {duration}", font=("Arial", 11)).pack(anchor="w", padx=20)
+        ctk.CTkLabel(frame, text=f" Weekly Lectures: {lectures}", font=("Arial", 11)).pack(anchor="w", padx=20)
+
+    ctk.CTkButton(subjects_frame, text="⬅️ Back to Details Page", command=lambda: showframe(info_frame), **button_config).pack(pady=10)
+
+
+def show_rooms_data():
+    for widget in room_frame.winfo_children():
+        widget.destroy()
+
+    ctk.CTkLabel(room_frame, text="All Rooms Info", font=("Arial", 16, "bold")).pack(pady=10)
+
+    scroll_frame = ctk.CTkScrollableFrame(room_frame, width=760, height=460)
+    scroll_frame.pack(padx=10, pady=5, fill="both", expand=True)
+
+    theory_rooms = room.get("normal_rooms", [])
+    lab_rooms = room.get("lab_rooms", [])
+
+    # Display theory rooms
+    theory_frame = ctk.CTkFrame(scroll_frame)
+    theory_frame.pack(pady=5, padx=10, fill="x")
+    ctk.CTkLabel(theory_frame, text="=> Normal Rooms:", font=("Arial", 12, "bold")).pack(anchor="w", padx=10)
+    ctk.CTkLabel(theory_frame, text=", ".join(theory_rooms)).pack(anchor="w", padx=20)
+
+    # Display lab rooms
+    lab_frame = ctk.CTkFrame(scroll_frame)
+    lab_frame.pack(pady=5, padx=10, fill="x")
+    ctk.CTkLabel(lab_frame, text="=> Lab Rooms:", font=("Arial", 12, "bold")).pack(anchor="w", padx=10)
+    ctk.CTkLabel(lab_frame, text=", ".join(lab_rooms)).pack(anchor="w", padx=20)
+
+    ctk.CTkButton(room_frame, text="⬅️ Back to Details page", command=lambda: showframe(info_frame),**button_config).pack(pady=10)
+
+
+
+
+ctk.CTkButton(button_container, text="👨‍🏫 Show Teachers Info", command=lambda: [show_teachers_data(), showframe(teacher_frame)],**h_button_config).pack(pady=10)
+ctk.CTkButton(button_container, text="🏷️ Show Classes Info", command=lambda: [show_class_data(), showframe(class_frame)],**h_button_config).pack(pady=10)
+ctk.CTkButton(button_container, text="📚 Show Subjects Info", command=lambda: [show_subject_data(), showframe(subjects_frame)],**h_button_config).pack(pady=10)
+ctk.CTkButton(button_container, text="🏢 Show Rooms Info", command=lambda: [show_rooms_data(), showframe(room_frame)],**h_button_config).pack(pady=10)
+
+showframe(home_frame)
 app.mainloop()
